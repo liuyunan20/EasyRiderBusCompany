a490991f2a6ccb08a6ad0168bbb178d683bde94d
liuyunan <liuyunan.20@gmail.com>
2022-05-01 00:42:54 -0700

    Regexp tasks authors just love making tasks about e-mail addresses. We are not an exception.
    Let's write a template matching an e-mail address. We'll consider the following format of valid e-mail addresses:
    
    its first part can contain from 6 to 30 characters. These characters can contain lowercase Latin letters (a-z), digits (0-9), hyphens -, dot characters . , equal signs = or underscores _
    the second part that immediately follows the first one can be either @gmail.com, @jetbrains.org or @hyperskill.org
    note that other combinations of the correct domain and top-level domain names are invalid. For example, @jetbrains.com or @gmail.org shouldn't be accepted by your regex.

---
+++ Topics/Groups and alternations/Advanced e-mail verification/main.py
@@ -0,0 +1,5 @@
+import re
+
+
+template = r'[a-z0-9\.=_-]{6,30}(@gmail\.com|@jetbrains\.org|@hyperskill\.org)'
+# print(re.match(r'[\.=_\-a-z0-9]{6,30}(@gmail\.com|@jetbrains\.org|@hyperskill\.org)', 'o=9.031yjrg@hyperskill.org'))
